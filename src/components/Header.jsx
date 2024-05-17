a5111f854415def697b882856618324e327d2cfb
shaurya35 <shauryajha35@gmail.com>
2024-05-17 21:01:53 +0530

    add navbar

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,11 +1,19 @@
 import React from 'react'
+import './Header.css'
 
 export default function Header() {
   return (
     <>
-        <div>
-            <h1>Header here</h1>
+      <header className='header'>
+        <div className="header_logo_envato">
+          <a href="" className='logo_envato_link'>
+            <img src="https://public-assets.envato-static.com/assets/logos/envato_market-dde916a48af7413e54aa058fe7ae6856752fca799da4bec355f00aea904bfa19.svg" alt="Envato" className='logo_envato_img'/>
+          </a>
         </div>
+        <div className="header_button">
+          <a href="" className="button_buynow">Buy now</a>
+        </div>
+      </header>
     </>
   )
 }
