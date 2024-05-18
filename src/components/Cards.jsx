1f9007451996100b2fd2fda0573c0e6cffb90393
shaurya35 <shauryajha35@gmail.com>
2024-05-18 16:38:07 +0530

    add footer1

--- src/components/Cards.jsx
+++ src/components/Cards.jsx
@@ -2,8 +2,19 @@ import React from 'react'
 
 export default function Cards() {
   return (
-    <div>
-      <h1>card</h1>
-    </div>
+    <>
+      <footer className="footer1_outer">
+        <div className="footer1">
+          <div className="footer1_text">An All-Round Plugin With <br />Powerful Features</div>
+          <div className="footer1_detail">Whether you're seasoned web developer or just starting out, Motion Art for <br /> Elementor seamlessly integrates with the Elementor platform, providing you <br />with a seamless and intuitive experiences.</div>
+        </div>
+      </footer>
+
+      <footer className="footer2_outer">
+        <div className="footer2">
+
+        </div>
+      </footer>
+    </>
   )
 }
