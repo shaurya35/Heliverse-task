d38c98511f416a1819bd96446ea4f569c22e26e7
shaurya35 <shauryajha35@gmail.com>
2024-05-19 00:05:04 +0530

    website now replicated

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,10 +1,18 @@
-import React from 'react'
-import './Footer.css'
+import React from "react";
+import "./Footer.css";
 
 export default function Footer() {
   return (
-    <div>
-      <h1>footer</h1>
-    </div>
-  )
+    <>
+      <footer className="footer__1__outer">
+        <div className="footer__1">
+          <div className="footer__1__content__1">© 2023 Copywrite. All rights reserved by QodeMatrix</div>
+          <div className="footer__1__content__2">
+            <div className="content__2__c1">Documentation</div>
+            <div className="content__2__c2">Support</div>
+          </div>
+        </div>
+      </footer>
+    </>
+  );
 }
